7efcc35042bfd6e79e686377d70da91fcc549f10
pseudoureka <messiah7179@gmail.com>
2024-10-15 10:54:14 +0900

    sort 를 이용한 정렬함 수 만들기

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
+import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleRatingClick = () => setOrder("rating");
+
   return (
     <div>
-      <ReviewList items={mockItems} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleRatingClick}>평점순</button>
+      </div>
+      <ReviewList items={sortedItems} />
     </div>
   );
 }

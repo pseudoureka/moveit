cd4b0b39228b36b859a1a785cf52d010facb4081
pseudoureka <messiah7179@gmail.com>
2024-10-17 17:59:24 +0900

    사이드이펙트 사용 방법, 정리방법, 이미지 URL 사용 createObjectURL, RevokeObjectURL

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,22 +1,28 @@
 import { useState } from "react";
 import "./ReviewForm.css";
+import FileInput from "./FileInput";
 
 const INITIAL_VALUE = {
   title: "",
   rating: 0,
   content: "",
+  imgFile: null,
 };
 
 function ReviewForm() {
   const [values, setValues] = useState(INITIAL_VALUE);
 
-  const handleChange = (e) => {
-    const { name, value, type } = e.target;
+  const handleChange = (name, value, type) => {
     setValues((prevValues) => ({
       ...prevValues,
       // [name]: value,
       [name]: sanitize(type, value),
     }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target;
+    handleChange(name, value, type);
   };
 
   function sanitize(type, value) {
@@ -40,9 +46,10 @@ function ReviewForm() {
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <input name="title" value={values.title} onChange={handleChange} />
-      <input name="rating" min={0} max={5} type="number" value={values.rating} onChange={handleChange} />
-      <textarea name="content" value={values.content} onChange={handleChange} />
+      <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
+      <input name="title" value={values.title} onChange={handleInputChange} />
+      <input name="rating" min={0} max={5} type="number" value={values.rating} onChange={handleInputChange} />
+      <textarea name="content" value={values.content} onChange={handleInputChange} />
       <button type="submit" disabled={!isFormValid}>
         확인
       </button>

47ad3021cb0624e9c7867d5141273d7c66f2c507
pseudoureka <messiah7179@gmail.com>
2024-10-28 23:17:34 +0900

    Context 를 함수로 관리하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { createReview, deleteReview, getReviews, updateReview } from "../api";
-import LocaleContext from "./contexts/LocaleContext";
+import { LocaleProvider } from "./contexts/LocaleContext";
 import useAsync from "./hooks/useAsync";
 import LocaleSelect from "./LocaleSelect";
 import ReviewForm from "./ReviewForm";
@@ -9,7 +9,6 @@ import { useCallback, useEffect, useState } from "react";
 const LIMIT = 6;
 
 function App() {
-  const [locale, setLocale] = useState("ko");
   const [order, setOrder] = useState("createdAt");
   const [items, setItems] = useState([]);
   const [offset, setOffset] = useState(0);
@@ -64,9 +63,9 @@ function App() {
   };
 
   return (
-    <LocaleContext.Provider value={locale}>
+    <LocaleProvider defaultValue={"en"}>
       <div>
-        <LocaleSelect value={locale} onChange={setLocale} />
+        <LocaleSelect />
         <div>
           <button onClick={handleNewestClick}>최신순</button>
           <button onClick={handleRatingClick}>평점순</button>
@@ -85,7 +84,7 @@ function App() {
         )}
         {loadingError?.message && <span>{loadingError.message}</span>}
       </div>
-    </LocaleContext.Provider>
+    </LocaleProvider>
   );
 }
 
--- src/components/contexts/LocaleContext.js
+++ src/components/contexts/LocaleContext.js
@@ -1,5 +1,38 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const LocaleContext = createContext();
 
-export default LocaleContext;
+export function LocaleProvider({ defaultValue = "ko", children }) {
+  const [locale, setLocale] = useState(defaultValue);
+
+  return (
+    <LocaleContext.Provider
+      value={{
+        locale,
+        setLocale,
+      }}
+    >
+      {children}
+    </LocaleContext.Provider>
+  );
+}
+
+export function useLocale() {
+  const context = useContext(LocaleContext);
+  console.log(context);
+  if (!context) {
+    throw new Error("반드시 LocaleProvider 안에서 만들어야 합니다.");
+  }
+
+  return context.locale;
+}
+
+export function useSetLocale() {
+  const context = useContext(LocaleContext);
+  console.log(context);
+  if (!context) {
+    throw new Error("반드시 LocaleProvider 안에서 만들어야 합니다.");
+  }
+
+  return context.setLocale;
+}

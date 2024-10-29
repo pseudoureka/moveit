de2c99bd209bdcc9e8351b481a9170f64c23ad25
pseudoureka <messiah7179@gmail.com>
2024-10-29 23:16:15 +0900

    mockdata 정렬하기, useState를 활용해서 최신순, 평점순 정렬버튼 만들기

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import ReviewList from "./ReviewList";
 import mockItems from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+
+  const sortedItems = mockItems.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestButton = () => setOrder("createdAt");
+  const handleBestButton = () => setOrder("rating");
+
   return (
     <div>
-      <ReviewList items={mockItems} />
+      <button onClick={handleNewestButton}>최신순</button>
+      <button onClick={handleBestButton}>평점순</button>
+      <ReviewList items={sortedItems} />
     </div>
   );
 }
